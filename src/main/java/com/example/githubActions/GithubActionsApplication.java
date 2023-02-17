25c84a8e2b44670afb553d246116d82e1c6b0100
TechVerito <techverito@192.168.1.43>
2023-02-17 17:22:55 +0530

    checking caching

--- src/main/java/com/example/githubActions/GithubActionsApplication.java
+++ src/main/java/com/example/githubActions/GithubActionsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GithubActionsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GithubActionsApplication.class, args);
 	}
 
